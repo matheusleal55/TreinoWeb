45c17a7b02299546681aceee965e1214c8ce0224
matheusleal55 <54157855+matheusleal55@users.noreply.github.com>
2021-03-16 22:04:11 -0300

    Add files via upload

---
+++ Controllers/AlunoController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAplicaçãoTreino.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            string[] alunos =
+            {
+                "José",
+                "João",
+                "maria"
+
+            };
+            return View(alunos);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAplicaçãoTreino.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
